84318e6456d3948f63c6d4759233da1b681cd3aa
Gordon <gordon.a.macpherson@gmail.com>
2018-02-28 16:45:57 +0000

    Adding new suite of unit tests

---
+++ NUnitTestSuite/Connections.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Net;
+using NUnit.Framework;
+using Lidgren;
+using Lidgren.Network;
+using NUnit.Framework.Constraints;
+
+namespace NUnitTestSuite
+{
+    [TestFixture]
+    public class Connections
+    {
+        public NetServer StartServer()
+        {
+            var config = new NetPeerConfiguration("test-suite")
+            {
+                EnableUPnP = true,
+                Port = 27015
+            };
+
+            // enable nat 
+            config.EnableMessageType(NetIncomingMessageType.NatIntroductionSuccess);
+
+            NetServer server = new NetServer(config);
+            server.Start();
+
+            return server;
+        }
+
+        public void StopServer( NetServer server )
+        {
+            server.Shutdown("closing server");
+        }
+
+        [Test, Repeat(100)]
+        public void NetworkServerTest()
+        {
+            var server = StartServer();
+            StopServer(server);
+
+            Assert.AreEqual(NetPeerStatus.ShutdownRequested, server.Status);
+            Assert.That(() => server.Status, Is.EqualTo(NetPeerStatus.NotRunning).After(2).Seconds.PollEvery(100));
+        }
+
+        [Test]
+        public void NetworkClientInitTest()
+        {
+            var config = new NetPeerConfiguration("test-suite")
+            {
+                EnableUPnP = true,
+                Port = 27015
+            };
+
+            config.EnableMessageType(NetIncomingMessageType.NatIntroductionSuccess);
+
+            var client = new NetClient(config);
+            client.Start();
+            client.Shutdown("closing client connection");
+        }
+    }
+}
