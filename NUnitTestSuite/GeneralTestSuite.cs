63101bdd11ea4062ca8bbb1a77e2cd62305cb957
Gordon Alexander MacPherson <gordon.a.macpherson@gmail.com>
2018-03-01 12:48:12 +0000

    Updated old test suite to NUnit, still work to do though.

---
+++ NUnitTestSuite/GeneralTestSuite.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+using NUnit.Framework;
+using UnitTests;
+
+namespace NUnitTestSuite
+{
+    [TestFixture]
+    class GeneralTestSuite
+    {
+        [Test]
+        public void GenericTestSuite()
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("oldUnitTests");
+            config.EnableUPnP = true;
+            NetPeer peer = new NetPeer(config);
+            peer.Start(); // needed for initialization
+
+            TestContext.Out.WriteLine("Unique identifier is " + NetUtility.ToHexString(peer.UniqueIdentifier));
+
+            ReadWriteTests.Run(peer);
+
+            NetQueueTests.Run();
+
+            MiscTests.Run(peer);
+
+            BitVectorTests.Run();
+
+            EncryptionTests.Run(peer);
+
+            var om = peer.CreateMessage();
+            peer.SendUnconnectedMessage(om, new IPEndPoint(IPAddress.Loopback, 14242));
+            try
+            {
+                peer.SendUnconnectedMessage(om, new IPEndPoint(IPAddress.Loopback, 14242));
+            }
+            catch (NetException nex)
+            {
+                if (nex.Message != "This message has already been sent! Use NetPeer.SendMessage() to send to multiple recipients efficiently")
+                    throw;
+            }
+
+            peer.Shutdown("bye");
+
+            // read all message
+            NetIncomingMessage inc = peer.WaitMessage(5000);
+            while (inc != null)
+            {
+                switch (inc.MessageType)
+                {
+                    case NetIncomingMessageType.DebugMessage:
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                    case NetIncomingMessageType.WarningMessage:
+                    case NetIncomingMessageType.ErrorMessage:
+                        TestContext.Out.WriteLine("Peer message: " + inc.ReadString());
+                        break;
+                    case NetIncomingMessageType.Error:
+                        throw new Exception("Received error message!");
+                }
+
+                inc = peer.ReadMessage();
+            }
+
+            TestContext.Out.WriteLine("Completed Generic Test Suite");
+        }
+    }
+}
